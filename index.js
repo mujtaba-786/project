6ce53e958e3dc320666dcb50c28926cf91f152c7
mujtaba-786 <mohdmujtaba11042000>
2023-06-25 21:20:42 -0400

    second

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log(1)
+var title = document.getElementById("header-title");
+var title2 = document.getElementById("title");
+
+title.style.borderBottom = "solid 6px black"; 
+title2.style.fontWeight = "bold";
+title2.style.color = "green";
