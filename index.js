3e3c17c318c1c5fdadfd8144e86d84a88bd369fe
mujtaba-786 <mohdmujtaba11042000>
2023-06-29 18:04:36 -0400

    third

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var title = document.getElementById("header-title");
-var title2 = document.getElementById("title");
+// var title = document.getElementById("header-title");
+// var title2 = document.getElementById("title");
 
-title.style.borderBottom = "solid 6px black"; 
-title2.style.fontWeight = "bold";
-title2.style.color = "green";
+
+// title.style.borderBottom = "solid 6px black"; 
+// title2.style.fontWeight = "bold";
+// title2.style.color = "green";
+
+
+var lis = document.getElementsByClassName("list-group-item")
+console.log(lis)
+
+lis[2].style.color = "red"
+
+for(var i=0;i<lis.length;i++){
+    lis[i].style.fontWeight = "bold"
+}
